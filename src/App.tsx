29f809a59b24bbd3afa4c80d005ce3b617eb4886
oleksandrkever <oleksandr.kever@fourmeta.com>
2024-01-10 10:43:59 +0200

    add Order Books

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 
 import './index.css'
+import OrderBooks from './pages/OrderBooks/OrderBooks'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl bg-slate-500 font-bold underline">
-      Hello world!
-    </h1>
+      <OrderBooks />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
-import OrderBooks from './pages/OrderBooks/OrderBooks.tsx';
+// import {
+//   createHashRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// import OrderBooks from './pages/OrderBooks/OrderBooks.tsx';
 
-const router = createHashRouter([
-  {
+// const router = createHashRouter([
+//   {
     
-    path: '/', 
-    element: <App />,
-  },
-  {
-    path: '/order-books',
-    element: <OrderBooks />,
-  }
-], {
-  basename: '/bible-books',
-}); 
+//     path: '/', 
+//     element: <App />,
+//   },
+//   {
+//     path: '/order-books',
+//     element: <OrderBooks />,
+//   }
+// ], {
+//   basename: '/bible-books',
+// }); 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <App />
   </React.StrictMode>,
 )
